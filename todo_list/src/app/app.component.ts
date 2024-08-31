fa7dd1c0f011bd4884aa674f501e36b7f8658bc1
Etie20 <etienne.foyang@2026.ucac-icam.com>
2024-08-31 03:17:53 +0100

    add login page

--- todo_list/src/app/app.component.ts
+++ todo_list/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {HlmButtonDirective} from "./shared/libs/ui/ui-button-helm/src";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HlmButtonDirective, HlmButtonDirective],
+  imports: [CommonModule, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- todo_list/src/app/pages/authentication/login/login.component.ts
+++ todo_list/src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {HlmButtonDirective} from "../../../shared/libs/ui/ui-button-helm/src";
+import {HlmInputDirective} from "../../../shared/libs/ui/ui-input-helm/src";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    HlmButtonDirective,
+    HlmInputDirective,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
