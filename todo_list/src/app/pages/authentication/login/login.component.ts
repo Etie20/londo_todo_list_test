7fc6ea923cd47afff2fb0ae65d5161bea4eec256
Etie20 <etienne.foyang@2026.ucac-icam.com>
2024-08-31 03:48:03 +0100

    add login page #3

--- todo_list/src/app/pages/authentication/login/login.component.ts
+++ todo_list/src/app/pages/authentication/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {HlmButtonDirective} from "../../../shared/libs/ui/ui-button-helm/src";
 import {HlmInputDirective} from "../../../shared/libs/ui/ui-input-helm/src";
+import {HlmSpinnerComponent} from "../../../shared/libs/ui/ui-spinner-helm/src";
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,11 @@ import {HlmInputDirective} from "../../../shared/libs/ui/ui-input-helm/src";
   imports: [
     HlmButtonDirective,
     HlmInputDirective,
+    HlmSpinnerComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loading = signal(false);
 }
