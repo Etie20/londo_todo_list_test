745cedae125238de5e58cb21ff112997c30de1af
Etie20 <etienne.foyang@2026.ucac-icam.com>
2024-08-31 04:14:09 +0100

    add register page

--- todo_list/src/app/pages/authentication/register/register.component.ts
+++ todo_list/src/app/pages/authentication/register/register.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {HlmButtonDirective} from "../../../shared/libs/ui/ui-button-helm/src";
+import {HlmInputDirective} from "../../../shared/libs/ui/ui-input-helm/src";
+import {HlmSpinnerComponent} from "../../../shared/libs/ui/ui-spinner-helm/src";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    HlmButtonDirective,
+    HlmInputDirective,
+    HlmSpinnerComponent
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  loading = signal(false)
 }
