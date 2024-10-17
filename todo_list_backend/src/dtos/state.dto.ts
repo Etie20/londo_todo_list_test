e8001580eb514d63d5c10f989a941eee6ffe4c0d
Etie20 <etienne.foyang@2026.ucac-icam.com>
2024-10-17 11:25:12 +0100

    fix: Dtos classes validator

--- todo_list_backend/src/dtos/state.dto.ts
+++ todo_list_backend/src/dtos/state.dto.ts
@@ -1,3 +1,6 @@
+import {IsString} from "class-validator";
 export class CreateBaseDto {
+    //@ts-ignore
+    @IsString()
     public name: string;
 }
--- todo_list_backend/src/dtos/task.dto.ts
+++ todo_list_backend/src/dtos/task.dto.ts
@@ -1,13 +1,21 @@
+import {IsString,IsDate} from "class-validator";
 export class CreateTaskDto {
+    //@ts-ignore
+    @IsString()
     public name: string;
 
+    @IsString()
     public description: string;
 
+    @IsString()
     public state: string;
 
+    @IsString()
     public category: string;
 
+    @IsDate()
     public created_at: Date;
 
+    @IsString()
     public user: string;
 }
