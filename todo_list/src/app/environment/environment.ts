cf34f2d18c74123fc8c13ff4ac8470d185b1308c
Etie20 <etienne.foyang@2026.ucac-icam.com>
2024-10-17 11:18:52 +0100

    fix: env file

--- todo_list/src/app/environment/environment.ts
+++ todo_list/src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   title: 'LONDO_TODO_LIST',
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'https://londo-todo-list-test.onrender.com/api/v1',
 };
